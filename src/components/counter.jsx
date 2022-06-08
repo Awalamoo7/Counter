1174b4c4be8e5d65083d988fd858a363b6d9973a
Awal Amoo <awalamoo7@gmail.com>
2022-06-09 00:45:46 +0100

    new dynamic rendering using classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state ={
-        count: 0,
+        count: 1,
         imageUrl: "https://picsum.photos/200"
     };
     
@@ -11,20 +11,27 @@ class Counter extends Component {
         fontWeight: 'bold'
     }
 
-    function (a) {
-        let x = 0;
-        x > 2 ? 'It is greater than two' : 'lesser than two' 
+    // function (a) {
+    //     let x = 0;
+    //     x > 2 ? 'It is greater than two' : 'lesser than two' 
         
-    }
+    // }
 
     render() { 
+        let classes = this.getBadgeClasses();
         return (
         <div>
             <img src = {this.state.imageUrl} alt ="Minimal image"/>
-            <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span style={this.styles} className={classes}>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2";
+        classes += (this.state.count === 0) ? " badge-warning" : " badge-primary";
+        return classes;
     }
 
     formatCount () {
