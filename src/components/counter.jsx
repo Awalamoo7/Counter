bc9737f93d817ef638bb4d44c1dd98f5c76daecd
Awal Amoo <awalamoo7@gmail.com>
2022-06-08 23:21:39 +0100

    added dynamic function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,10 +11,16 @@ class Counter extends Component {
         fontWeight: 'bold'
     }
 
+    function (a) {
+        let x = 0;
+        x > 2 ? 'It is greater than two' : 'lesser than two' 
+        
+    }
+
     render() { 
         return (
         <div>
-            <img src = {this.state.imageUrl} alt =""/>
+            <img src = {this.state.imageUrl} alt ="Minimal image"/>
             <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </div>
