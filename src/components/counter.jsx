f7b9a202ec78c7f914bba1ff0590834ad39e4b5b
Awal Amoo <awalamoo7@gmail.com>
2022-06-10 00:35:25 +0100

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,6 +12,12 @@ class Counter extends Component {
         fontWeight: 'bold'
     }
 
+    renderTags() {
+        if (this.state.tags.length === 0) return null;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    }
+
     // function (a) {
     //     let x = 0;
     //     x > 2 ? 'It is greater than two' : 'lesser than two' 
@@ -19,27 +25,28 @@ class Counter extends Component {
     // }
 
     render() { 
-        let classes = this.getBadgeClasses();
+        // let classes = this.getBadgeClasses();
         return (
         <div>
             <img src = {this.state.imageUrl} alt ="Minimal image"/>
-            <span style={this.styles} className={classes}>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
-            <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+            {/* <span style={this.styles} className={classes}>{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button> */}
+            {this.state.tags.length === 0 && 'There are no tags!'}
+            {this.renderTags()}
         </div>
         );
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2";
-        classes += (this.state.count === 0) ? " badge-warning" : " badge-primary";
-        return classes;
-    }
+    // getBadgeClasses() {
+    //     let classes = "badge m-2";
+    //     classes += (this.state.count === 0) ? " badge-warning" : " badge-primary";
+    //     return classes;
+    // }
 
-    formatCount () {
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+    // formatCount () {
+    //     const {count} = this.state;
+    //     return count === 0 ? 'Zero' : count;
+    // }
 }
  
 export default Counter;
