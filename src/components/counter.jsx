1fb5d8de4edaa19d413876ed9de14eabd4ea34db
Awal Amoo <awalamoo7@gmail.com>
2022-11-08 22:59:07 +0000

    update-react-state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state ={
-        count: 1,
-        imageUrl: "https://picsum.photos/200",
-        tags: ['tag1', 'tag2', 'tag3']
+        count: 0,
     };
     
     styles = {
@@ -12,44 +10,31 @@ class Counter extends Component {
         fontWeight: 'bold'
     }
 
-    renderTags() {
-        if (this.state.tags.length === 0) return null;
-
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    handleIncrement = () => {
+        this.setState({count: this.state.count + 1})
     }
-
-    defaultTag = () => console.log(`improve the {this.state.count}`)
-
-    // function (a) {
-    //     let x = 0;
-    //     x > 2 ? 'It is greater than two' : 'lesser than two' 
-        
-    // }
-
     render() { 
-        // let classes = this.getBadgeClasses();
         return (
         <div>
-            <img src = {this.state.imageUrl} alt ="Minimal image"/>
-            {/* <span style={this.styles} className={classes}>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button> */}
-            {this.state.tags.length === 0 && 'There are no tags!'}
-            {this.renderTags()}
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button 
+            onClick={this.handleIncrement}
+            className='btn btn-secondary btn-sm'>Increment</button>
         </div>
         );
 
     }
 
-    // getBadgeClasses() {
-    //     let classes = "badge m-2";
-    //     classes += (this.state.count === 0) ? " badge-warning" : " badge-primary";
-    //     return classes;
-    // }
+    getBadgeClasses() {
+        let classes = "badge m-2";
+        classes += (this.state.count === 0) ? " badge-warning" : " badge-primary";
+        return classes;
+    }
 
-    // formatCount () {
-    //     const {count} = this.state;
-    //     return count === 0 ? 'Zero' : count;
-    // }
+    formatCount () {
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
 }
  
 export default Counter;
