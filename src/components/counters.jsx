cb30ec760fd4962d9995e6e4c4ecd78c651b5355
Awal Amoo <awalamoo7@gmail.com>
2022-11-16 00:37:45 +0000

    updating state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,7 +5,7 @@ class Counters extends Component {
     state = { 
         counters: [
             {id:1, value:0},
-            {id:2,value:4},
+            {id:2, value:0},
             {id:3, value:0},
             {id:4, value:0}
         ]
@@ -15,12 +15,36 @@ class Counters extends Component {
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({counters});
     }
+    handleIncrement = counter => {
+        const counters = [...this.state.counters]
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    }
+    handleReset = counter => {
+        const counters = this.state.counters.map(c=> {
+            c.value = 0;
+            return c;
+        });
+        this.setState(counters);
+    }
 
     render() { 
         return (
             <div>
+                <button 
+                onClick={this.handleReset}
+                className='btn btn-primary btn-sm m-2'
+            >
+                Reset
+            </button>
                 {this.state.counters.map(counter => (
-                    <Counter key={counter.id} onDelete={this.handleDelete} counter = {counter}/>
+                    <Counter 
+                    key={counter.id} 
+                    onIncrement={this.handleIncrement}
+                    onDelete={this.handleDelete} 
+                    counter = {counter}/>
                 ))}
             </div>
         );
