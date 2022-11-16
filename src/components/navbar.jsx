44e13a11042b45928d9d003f1433bded34e36917
Awal Amoo <awalamoo7@gmail.com>
2022-11-16 19:38:07 +0000

    restructuring stateless component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return (<nav className="navbar bg-light">
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Navbar</span>
-        </div>
-      </nav>);
-    }
+const NavBar = ({totalCounters}) => {
+    return (
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">
+                Navbar 
+                <span className='badge badge-pill badge-secondary m-2'>{totalCounters}</span>
+            </span>
+            </div>
+        </nav> 
+     );
 }
  
-export default NavBar;
+export default NavBar; 
+
