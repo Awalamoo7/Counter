8b438ae6efb735eb75a4853a472956721998457a
Awal Amoo <awalamoo7@gmail.com>
2022-11-16 01:07:07 +0000

    adding and styling navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() { 
+        return (<nav className="navbar bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>);
+    }
+}
+ 
+export default NavBar;
